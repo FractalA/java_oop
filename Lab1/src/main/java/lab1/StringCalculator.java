61fdb10b2a510c5d56d186243dbb806901816cec
Dmytro Saienko <saenko26051978@gmail.com>
2023-10-20 20:31:25 +0300

    first step

---
+++ Lab1/src/main/java/lab1/StringCalculator.java
@@ -0,0 +1,32 @@
+package lab1;
+import java.util.Scanner;
+
+
+public class StringCalculator {
+    public static int Add(String numbers) {
+        if (numbers.isEmpty()) {
+            return 0;
+        }
+        String[] numArr = numbers.split(",");
+        int res = 0;
+        for (String num : numArr) {
+            num = num.trim();
+            try {
+                int x = Integer.parseInt(num);
+                res += x;
+            } catch (NumberFormatException e) {
+                System.err.println("помилка: недійсне число ігнорується.");
+            }
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("введіть рядок: ");
+        String input = scanner.nextLine();
+        int res = Add(input);
+        System.out.println("результат: "+res);
+        scanner.close();
+    }
+}
