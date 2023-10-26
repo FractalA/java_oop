60ef5872a83997c5fe2954113d0e2d01e9ccdbbe
Dmytro Saienko <saenko26051978@gmail.com>
2023-10-26 19:09:21 +0300

    4 step

--- Lab1/src/main/java/lab1/StringCalculator.java
+++ Lab1/src/main/java/lab1/StringCalculator.java
@@ -1,13 +1,27 @@
 package lab1;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class StringCalculator {
     public static int Add(String numbers) {
         if (numbers.isEmpty()) {
             return 0;
         }
-        String[] delimiters = { ",|\\\\n" };
-        String[] numArr = numbers.split(String.join("|", delimiters));
+
+        String delimiter = ",|\\\\n";
+
+        if (numbers.startsWith("//")) {
+            Pattern pattern = Pattern.compile("//(.*?)\\\\n");
+            Matcher matcher = pattern.matcher(numbers);
+            if (matcher.find()) {
+                String customDelimiter = matcher.group(1);
+                delimiter = Pattern.quote(customDelimiter) + "|\\\\n|,";
+                numbers = numbers.substring(matcher.end());
+            }
+        }
+
+        String[] numArr = numbers.split(delimiter);
         int res = 0;
         for (String num : numArr) {
             num = num.trim();
@@ -26,7 +40,7 @@ public class StringCalculator {
         System.out.print("введіть рядок: ");
         String input = scanner.nextLine();
         int res = Add(input);
-        System.out.println("результат: "+res);
+        System.out.println("результат: " + res);
         scanner.close();
     }
 }
